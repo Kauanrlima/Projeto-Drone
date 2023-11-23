19b26ca8e6c974598c02a136b8f4a9bca0a8ef51
Kauanrlima <149845097+Kauanrlima@users.noreply.github.com>
2023-11-23 11:09:58 -0300

    some pygames tests

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 ### Main code of the project
+
+import pygame as pg
+from pygame.locals import (K_UP,K_DOWN,K_LEFT,K_RIGHT)
+
+class keyboard:
+    def __init__(self):
+        self.pg.init()
+        self.pressed_keys = pg.key.get_pressed()
+        screen = pg.display.set_mode((400,400))
+    
+    run = True
+    while run:
+        for event
+    
