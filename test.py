9fe69f034741f9cb6875279ff349524765a4bf26
Kauanrlima <149845097+Kauanrlima@users.noreply.github.com>
2023-11-23 22:48:39 -0300

    new pygame test

--- test.py
+++ test.py
@@ -6,23 +6,21 @@ import pygame as pg
 class keyboard:
     def __init__(self):
         self.pg.init()
-        self.pressed_keys = pg.key.get_pressed()
-        self.pg.quit()
-
+    
     screen = pg.display.set_mode((400,400))
     
-    run = True
-    while run:
-         for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
-
-            if event.type == pg.KEYDOWN:
-                if event.key == pg.K_UP:
-                    print('w')
-                elif event.key == pg.K_DOWN:
-                    print('s')
-                elif event.key == pg.K_LEFT:
-                    print('a')
-                elif event.key == pg.K_RIGHT:
-                    print('d')
+    running = True
+    while running:
+        for event in pg.event.get():
+         if event.type == pg.QUIT:
+             running = False
+        if event.type == pg.KEYDOWN:
+            keys = pg.key.get_pressed()
+            if keys[pg.K_LEFT]:
+                print('a')
+            if keys[pg.K_RIGHT]:
+                print('d')
+            if keys[pg.K_UP]:
+                print('w')
+            if keys[pg.K_DOWN]:
+                print('s')
