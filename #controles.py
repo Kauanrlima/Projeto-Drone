b39c119b087bbc8c6b5d0c641cbe912143d8ccf0
Kauanrlima <149845097+Kauanrlima@users.noreply.github.com>
2023-11-24 08:42:04 -0300

    assigned keys to velocity values

--- #controles.py
+++ #controles.py
@@ -1,6 +1,7 @@
 #controles 
 from djitellopy import tello
 from test import keyboard as kb
+import test as kb
 
 dr = tello.Tello()
 dr.connect()
@@ -9,5 +10,6 @@ print(dr.get_battery())
 dr.takeoff()
 
 
-while True:
-    dr.send_rc_control(0,0,0,0)
+
+vals = kb.KeyBoardInput()
+dr.send_rc_control(vals[0],vals[1],vals[2],vals[3])
--- test.py
+++ test.py
@@ -7,20 +7,31 @@ class keyboard:
     def __init__(self):
         self.pg.init()
     
-    screen = pg.display.set_mode((400,400))
+    screen = pg.display.set_mode((100,100))
+    
     
     running = True
     while running:
         for event in pg.event.get():
-         if event.type == pg.QUIT:
-             running = False
-        if event.type == pg.KEYDOWN:
-            keys = pg.key.get_pressed()
-            if keys[pg.K_LEFT]:
-                print('a')
-            if keys[pg.K_RIGHT]:
-                print('d')
-            if keys[pg.K_UP]:
-                print('w')
-            if keys[pg.K_DOWN]:
-                print('s')
+                if event.type == pg.QUIT:
+                 running = False
+                if event.type == pg.KEYDOWN:
+                    def KeyBoardInput():
+                        lr, fb, up, yv = 0,0,0,0
+                        speed = 20
+                        keys = pg.key.get_pressed()
+                        if keys[pg.K_LEFT]:
+                            lr = -speed
+                        if keys[pg.K_RIGHT]:
+                            lr = speed
+                        if keys[pg.K_UP]:
+                            fb = speed
+                        if keys[pg.K_DOWN]:
+                            fb = -speed
+                        return [lr,fb]
+                    print(KeyBoardInput())
+    
+   
+    
+        
+        
