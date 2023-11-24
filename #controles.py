4695e02562e98488e8a818c1cbdf3a41f8f0a751
Kauanrlima <149845097+Kauanrlima@users.noreply.github.com>
2023-11-24 08:08:21 -0300

    drone stuff

--- #controles.py
+++ #controles.py
@@ -2,4 +2,12 @@
 from djitellopy import tello
 from test import keyboard as kb
 
-print(kb)
+dr = tello.Tello()
+dr.connect()
+print(dr.get_battery())
+
+dr.takeoff()
+
+
+while True:
+    dr.send_rc_control(0,0,0,0)
