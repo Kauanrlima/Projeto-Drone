3d9c6990842f4ad7553f134d01228e52b66bdb32
Kauanrlima <149845097+Kauanrlima@users.noreply.github.com>
2023-12-04 17:18:37 -0300

    message to Mario

--- control.py
+++ control.py
@@ -1,18 +1,19 @@
-k = "kauan acho que funcionou"
+M = "como tu testou ele? a e tbm eu mexi naquele código q o eleno falou e resolvi o problema, agr vou tentar modificar pra keyboard"
 
 import cv2
 import time
 import numpy as np
 from djitellopy import tello
 
-def drone(self):
-    dr = tello.Tello()
-    dr.connect()
-    print(dr.get_battery())
-    dr.streamon()
-    dr.takeoff() 
-    dr.send_rc_control(0,0,0,0)
-    time.sleep(2.2)
+
+dr = tello.Tello()
+dr.connect()
+print(dr.get_battery())
+
+dr.streamon()
+dr.takeoff() 
+dr.send_rc_control(0,0,0,0)
+time.sleep(2.2)
 
 
 fbRange = [6200,6800]
@@ -64,16 +65,16 @@ def trackFace(me,info,w,pid,pError):
         speed = 0
         error = 0
         
-    drone.send_rc_control(0,fb,0,speed)
+    dr.send_rc_control(0,fb,0,speed)
       
 
 while True:
-    img = drone.get_frame_read().frame
+    img = dr.get_frame_read().frame
     img =cv2.resize(img,(w,h)) 
     img, info = findFace(img)
     pError = trackFace(info,w,pid,pError)
     print("center",info[0], "Area",info[1])
     cv2.imshow("Output", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        drone.land()
+        dr.land()
         break
